4248d0c2341b425599711583df3b9189c017367c
ajmalsamad <ajmal.samad@crystawall.com>
2025-07-05 15:09:28 +0400

    fix

--- src/services/BetService.js
+++ src/services/BetService.js
@@ -6,14 +6,14 @@ const fetchBetsByMatchAndUser = async (matchId, userId) => {
     throw new Error("Invalid match ID");
   }
   if (!userId || isNaN(userId)) {
-    throw new Error("Invalid user ID");
+    // throw new Error("Invalid user ID");
   }
 
   return await BetsModal.getBetsByMatchAndUser(matchId, userId);
 };
 const fetchUserBets = async (user_id, match_id) => {
   if (isNaN(user_id) || isNaN(match_id)) {
-    throw new Error("Invalid IDs");
+    // throw new Error("Invalid IDs");
   }
   return await BetsModal.getUserBetsByMatch(user_id, match_id);
 };
